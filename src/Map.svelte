16be649a3660d3abf93bc71238ca3ae22c6457a8
PavelDenisov1 <paveldenisovqwe01@mail.ru>
2023-09-08 15:58:04 +0400

    added the functionality of printing a map without displaying it

--- src/Map.svelte
+++ src/Map.svelte
@@ -5,26 +5,10 @@
     import OSM from 'ol/source/OSM.js';
     import 'ol/ol.css';
     import { onMount } from 'svelte';
-    import Print from "ol-ext/control/Print.js";
-    import { srcImage } from './store/data';
+    import { addPrintControl, initMapOnDoc, makePrintClick } from './utils';
 
-    let map: Map;
-
-    function addPrintControl(){
-        let printControl = new Print({ title: "Печать", imageType: "image/png" });
-        map.addControl(printControl);
-
-        printControl.on(['print', 'error'], function(e) {
-            if (e.image) {
-                $srcImage = e.image;
-            } else {
-                console.warn('No canvas to export');
-            }
-        });
-    }
-
-    onMount(() => {    
-        map = new Map({
+    onMount(() => {
+        const map = new Map({
             view: new View({
                 center: [0, 0],
                 zoom: 1,
@@ -33,24 +17,11 @@
                 new TileLayer({
                     source: new OSM(),
                 }),
-            ],
-            target: 'map',
+            ]
         });
 
-        addPrintControl();
+        addPrintControl(map);
+        initMapOnDoc(map);
+        makePrintClick(map);
     }); 
 </script>
-
-<div id="map" class="map"/>
-
-<style>  
-    .map {
-        width: 100%;
-        height: 100%;
-    }    
-
-    :global(.ol-print){
-        top: 0.5em;
-        right: 0.5em;
-    }
-</style>
---
+++ src/utils.ts
@@ -0,0 +1,51 @@
+import Map from 'ol/Map.js';
+import Print from "ol-ext/control/Print.js";
+import { srcImage } from './store/data';
+
+function deleteMapFromDoc(){
+    const mainContent = document.getElementById('main-content');
+    const div = document.getElementById('map');
+    mainContent.removeChild(div);
+}
+
+export function addPrintControl(map: Map){
+    if(map){
+        let printControl = new Print({ title: "Печать", imageType: "image/png" });
+        map.addControl(printControl);
+
+        printControl.on(['print', 'error'], function(e) {
+            if (e.image) {
+                srcImage.update(updater => updater.concat(e.image));
+                deleteMapFromDoc();
+            } else {
+                console.warn('No canvas to export');
+            }
+        });
+    }
+}
+
+export function makePrintClick(map: Map){
+    if(map){
+        let divOlPrint = map.getControls().getArray().find(i => i.constructor.name === 'olcontrolPrint');
+        if(divOlPrint){
+            const button = divOlPrint.element.children[0];
+            try{
+                button.click();
+            }
+            catch(e){
+                console.log(e);
+            }
+        }
+    }
+}
+
+export function initMapOnDoc(map: Map){
+    if(map){
+        const mainContent = document.getElementById('main-content');
+        const div = document.createElement('div');
+        div.style.visibility = 'hidden';
+        div.setAttribute("id", "map");
+        map.setTarget(div);
+        mainContent.appendChild(div);
+    }
+}
